df636621aef771114326787d5bae33c820957b1f
gabriellasantosss <gabriellaplsantos@gmail.com>
2025-07-28 21:28:56 +0000

    inicio

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,6 @@ This is a temporary script file.
 import streamlit as st
 
 st.set_page_config(page_title=f"cni-internacional")
-
-from utils import style_fragment
-
-with st.sidebar:
-    style_fragment()
-
-from func import check_password
-
-
 st.write("# **Inteligência Comercial 📊**")
 
 
