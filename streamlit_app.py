cc572f26806bfa3b80aed2c2d0d5dd47935c096a
gabriellasantosss <gabriellaplsantos@gmail.com>
2025-07-28 17:52:07 -0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import streamlit as st
+
+st.title("Olá, mundo!")
+st.write("Meu primeiro app no Streamlit.")
+
