ed7f9218921f4faa0cc0932869cb75c446760532
gabriellasantosss <gabriellaplsantos@gmail.com>
2025-07-29 12:59:39 +0000

    tentativa logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,22 @@ Spyder Editor
 This is a temporary script file.
 """
 import streamlit as st
+import locale
+import altair as alt
+from streamlit.components.v1 import html
+import json
+
 
 st.set_page_config(page_title=f"cni-internacional")
 st.write("# **Inteligência Comercial 📊**")
 
+# Função logo CNI
+def add_logo():
+
+    st.image(
+        "https://staticportaldaindustria.azureedge.net/static/img/logos/atualizado/cni.svg",
+        width=280,
+    )
 
 
 
